120d0ce7eb149af805d716fea8b71cd6e66eecb5
siddharth prabhu <siddharthprabhuu@gmail.com>
2024-04-27 23:46:11 +0530

    added loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,45 @@
 import "./App.css";
+import React, { useState, useEffect } from 'react';
+import { ClipLoader, HashLoader } from 'react-spinners';
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import ImageSlider from "./components/ImageSlider";
 import Spline from '@splinetool/react-spline';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true); // Initial state is loading
+  let [color, setColor] = useState("#ffffff");
+
+
+  const handleLoad = () => {
+    setTimeout(()=>{
+      setIsLoading(false)
+    },2000)
+    
+  }
+
   return (
     <div className='bg-black relative'>
-      <div className=" fixed top-0 left-0">
-      <Spline scene="https://prod.spline.design/MV0Xi4FW1FoKONyI/scene.splinecode" />
-      </div>
-      <div className="absolute w-full h-full top-0 left-0 z-10 bg-black/0 pointer-events-none">
-        <Navbar />
-        <Hero />
-        <ImageSlider />
+      {isLoading && (
+        <div className="flex w-full h-[100vh] justify-center items-center">
+          <HashLoader
+            color={color}
+            loading={isLoading}
+            size={150}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        </div>
+      )}
+      <div className="relative w-full h-full">
+        <div className=" fixed top-0 left-0">
+          <Spline scene="https://prod.spline.design/MV0Xi4FW1FoKONyI/scene.splinecode" />
+        </div>
+        <div className="absolute w-full h-full top-0 left-0 z-10 bg-black/0 pointer-events-none">
+          <Navbar />
+          <Hero onLoad={handleLoad} />
+          <ImageSlider />
+        </div>
       </div>
 
     </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import Spline from '@splinetool/react-spline';
 
 
-function Hero() {
+function Hero({ onLoad }) {
   return (
     <div className='relative overflow-x-hidden top-20 lg:top-0 pointer-events-auto'>
-      <Spline scene="https://prod.spline.design/o8yT4Fx-u9iu114N/scene.splinecode" />
+      <Spline onLoad={onLoad} scene="https://prod.spline.design/o8yT4Fx-u9iu114N/scene.splinecode" />
     </div>
   )
 }
